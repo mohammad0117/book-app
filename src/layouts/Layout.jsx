9246799f8f6a8ff77d568221ec064aa0e389d6d4
mohammad molavi <mohamadmolavi98@gmail.com>
2024-03-18 10:42:10 +0330

    create layout component and completed that

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./Layout.module.css"
+function Layout({children}) {
+  return (
+    <>
+    <header className={styles.header}>
+        <h1>Book App</h1>
+        <p><a href="https://botostart.ir">Botostart</a> React.js Full Course</p>
+    </header>
+    {children}
+    <footer className={styles.footer}>
+        <p>Developed By Milad With ❤️</p>
+    </footer>
+    </>
+  )
+}
+
+export default Layout
